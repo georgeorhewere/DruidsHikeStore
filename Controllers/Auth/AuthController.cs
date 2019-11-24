6a426441daacac1c9a0ffea216b739e330b8a8a3
george.orhewere@gmail.com <george.orhewere@gmail.com>
2019-11-24 02:43:14 +0100

    implemented register endpointto submit user registeration form from front end

--- Controllers/Auth/AuthController.cs
+++ Controllers/Auth/AuthController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DruidsHikeStore.Controllers.Auth
@@ -11,13 +12,43 @@ namespace DruidsHikeStore.Controllers.Auth
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private UserManager<StoreDB.StoreUser> _userManager;
+        public AuthController(UserManager<StoreDB.StoreUser> userManager)
+        {
+            this._userManager = userManager;
+
+
+        }
 
 
 
         [HttpPost("register", Name ="Register")]
-        public ActionResult<StoreDB.StoreUser> Register([FromBody] StoreDB.StoreUser value)
+        public async Task<ActionResult<Boolean>> Register([FromBody] StoreDB.ViewModels.AddUserViewModel value)
         {
-            return value;
+            if (ModelState.IsValid)
+            {
+                var storeUser = new StoreDB.StoreUser()
+                {
+                    FirstName = value.FirstName,
+                    LastName = value.LastName,
+                    Email = value.Email,
+                    UserName = value.Email,
+                    Address = value.Address,
+                    City = value.City,
+                    Country = value.Country
+                };
+                try
+                {
+                    var result = await _userManager.CreateAsync(storeUser, value.Password);
+
+                    return result.Succeeded;
+                }
+                catch (Exception ec) {
+                    return false;
+                }
+            }
+            return false;
+
         }
     }
         
--- StoreDB/ViewModels/AddUserViewModel.cs
+++ StoreDB/ViewModels/AddUserViewModel.cs
@@ -1,26 +1,29 @@
-﻿using Microsoft.AspNetCore.Identity;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
+using System.Text;
 
-namespace StoreDB
+namespace StoreDB.ViewModels
 {
-    public class StoreUser : IdentityUser
+    public class AddUserViewModel
     {
         private string firstName;
         private string lastName;
+        private string email;
         private string address;
         private string city;
         private string country;
+        private string password;
+        private string confirmPassword;
+        private int role;
+
         public string FirstName { get => firstName; set => firstName = value; }
         public string LastName { get => lastName; set => lastName = value; }
         public string Address { get => address; set => address = value; }
         public string City { get => city; set => city = value; }
         public string Country { get => country; set => country = value; }
+        public string Password { get => password; set => password = value; }
+        public string ConfirmPassword { get => confirmPassword; set => confirmPassword = value; }
+        public int Role { get => role; set => role = value; }
+        public string Email { get => email; set => email = value; }
     }
-
-
-   
 }
